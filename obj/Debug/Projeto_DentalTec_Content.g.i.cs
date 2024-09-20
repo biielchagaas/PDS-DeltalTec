a7b57c6369d5de8a4e59347c2b1bbf4acd52e4c0
samanthanovaes <novaes.samantha@estudante.ifro.edu.br>
2024-09-20 12:14:09 -0400

    primeirocommmit(isabelly)
    telas de relatorio de consulta, funcionario, caixa e paciente.
    
    foram feitas pela isabelly pimentel, mas que nao pôde commitar devido a alguns problemas com seu github

--- obj/Debug/Projeto_DentalTec_Content.g.i.cs
+++ obj/Debug/Projeto_DentalTec_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenslogin/padlock.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagenslogin/lupa2.png")]
 
 
