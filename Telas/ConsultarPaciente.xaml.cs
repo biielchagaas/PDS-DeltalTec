34d69c685c35a5729417aaa4510a5a5310da985a
Gabriel da Silva Chagas <2021102020058@aluno.ifro.edu.br>
2024-09-24 09:21:52 -0400

    commit antes do merge

---
+++ Telas/ConsultarPaciente.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projeto_DentalTec.Telas
+{
+    /// <summary>
+    /// Interação lógica para ConsultarPaciente.xam
+    /// </summary>
+    public partial class ConsultarPaciente : Page
+    {
+        public ConsultarPaciente()
+        {
+            InitializeComponent();
+        }
+    }
+}
