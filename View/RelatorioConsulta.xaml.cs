643bda338b35bc80c4b26eee4b4afc78d65e0e52
martin-lopes <martinrodrigueslopess@gmail.com>
2024-10-18 19:17:43 -0400

    OitavaVersao
    Correção das telas
    Versão PADRONIZADA

--- View/RelatorioConsulta.xaml.cs
+++ View/RelatorioConsulta.xaml.cs
@@ -23,10 +23,5 @@ namespace DentalTech.View
         {
             InitializeComponent();
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
-    
 }
