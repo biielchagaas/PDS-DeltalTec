d7117b9bd7ed89bfd3a7ca0b9a7f621562b26ff4
Gabriel da Silva Chagas <gabriel14chagas@gmail.com>
2024-12-13 14:27:45 -0400

    Servico iniciado
    codigo até o momento incompleto, esta faltando a parte de orçamento e a tela também

--- View/CadastrarServico.xaml.cs
+++ View/CadastrarServico.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DentalTech.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DentalTech.Database;
+using System.Globalization;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Windows.Markup;
 
 namespace DentalTech.View
 {
@@ -22,6 +28,111 @@ namespace DentalTech.View
         public CadastrarServico()
         {
             InitializeComponent();
+            PreencherComboBox();
         }
-    }
+        void PreencherComboBox()
+        {
+            try
+            {
+                Conexao conexao = new Conexao();
+                string query = "SELECT nome_func FROM Funcionario";
+
+                using (MySqlCommand command = conexao.Query(query))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Adiciona cada registro ao ComboBox
+                            profissionais.Items.Add(reader["nome_func"].ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao acessar o banco de dados: " + ex.Message);
+            }
+        }
+
+        private void Salvar(object sender, RoutedEventArgs e)
+        {
+            Inserir();
+        }
+
+        void Inserir()
+        {
+            string servicoo = servico.Text;
+            string desc = descricao.Text;
+
+            try
+            {
+
+                Conexao conexao = new Conexao();
+                int orcamento = -1;
+                string query = "SELECT id_orca FROM Orcamento WHERE nome_pac = @Nome";
+
+                using (MySqlCommand command = conexao.Query(query))
+                {
+                    // Adiciona o parâmetro de forma segura
+                    command.Parameters.AddWithValue("@Nome", orcamento);
+
+                    // Executa o comando e obtém o resultado
+                    object resultado = command.ExecuteScalar();
+
+                    if (resultado != null)
+                    {
+                        idPaciente = Convert.ToInt32(resultado);
+                        Console.WriteLine("ID encontrado: " + orcamento);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhum registro encontrado com esse nome.");
+                    }
+                }
+
+                int profissional = -1;
+                query = "SELECT id_func FROM Funcionario WHERE nome_func = @Nome";
+
+                using (MySqlCommand command = conexao.Query(query))
+                {
+                    command.Parameters.AddWithValue("@Nome", profissional);
+
+                    object resultado = command.ExecuteScalar();
+
+                    if (resultado != null)
+                    {
+                        profissional = Convert.ToInt32(resultado);
+                        Console.WriteLine("ID encontrado: " + profissional);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhum registro encontrado com esse nome.");
+                    }
+                }
+
+                query = "INSERT INTO Servico (nome_serv, descricao_serv, id_func_fk, id_orca_fk) VALUES (@servico, @descricao, @profissional, @orcamento)";
+
+                using (MySqlCommand command = conexao.Query(query))
+                {
+                    command.Parameters.AddWithValue("@servico", servicoo);
+                    command.Parameters.AddWithValue("@desc", desc);
+                    command.Parameters.AddWithValue("@profissional", profissional);
+                    command.Parameters.AddWithValue("@profissional", orcamento);
+                    var result = command.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Serviço cadastrado com sucesso.", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao cadastrar serviço.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao acessar o banco de dados: " + ex.Message);
+            }
+        }
 }
